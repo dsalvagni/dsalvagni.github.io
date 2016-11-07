c6e10f6e128db157dd73c7f6e7751e1decf4e172
Daniel Salvagni <danielsalvagni@gmail.com>
2016-11-07 18:24:11 -0200

    Add files via upload

--- frutalica/assets/js/main.js
+++ frutalica/assets/js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
 
     /**
      *  Alterar o plano selecionado
@@ -45,7 +45,7 @@ $(function () {
      *
      *
      */
-    $('.plan--options a:not([href$="html"])').on('click', function () {
+    $('.plan--options a:not([href$="html"])').on('click',function() {
         var elm = $(this);
         $('#plan-container').removeClass('p m g').addClass(elm.data('option'));
     });
@@ -53,7 +53,7 @@ $(function () {
     /**
      *  Alterar o ponto de coleta
      */
-    $('#selected-point').on('change', function () {
+    $('#selected-point').on('change',function() {
         var value = $(this).find(':selected').data('option');
         var marker = mkConfig.getMarker(value);
         mkConfig.setMarker(value)
@@ -64,18 +64,18 @@ $(function () {
      *  Selecionar a forma de entrega
      */
     function changeDeliveryMethod(value) {
-        if (value === 'retirar') {
+        if(value === 'retirar') {
             mapConfig();
             $('#delivery-method-receber').addClass('hide');
             $('#delivery-method-retirar').removeClass('hide');
             $('#selected-point').val($('#selected-point option:first').val());
-        } else if (value === 'receber') {
+        } else if(value === 'receber') {
             $('#delivery-method-retirar').addClass('hide');
             $('#delivery-method-receber').removeClass('hide');
         }
     }
 
-    $('[name="accept-terms"]').on('change', checkAcceptedTerms)
+    $('[name="accept-terms"]').on('change',checkAcceptedTerms)
 
     function checkAcceptedTerms() {
         var e = $(this);
@@ -86,17 +86,32 @@ $(function () {
         }
     }
 
+    $('.overlay .btn-close').on('click',hideModal);
+    $('.month-detail-header .btn-close').on('click',function() {
+        $('.month-detail .detail').hide();
+    });
+    $('.months a').on('click',function() {
+        var month = $(this).data('month');
+        $('.month-detail .detail').hide();
+        $('.months .primary-button-active').removeClass('primary-button-active');
+        $(this).addClass('primary-button-active');
+        $('#month-'+month).show();
+    });
+
     /**
      * Seleciona a forma de entrega na tela de planos
      */
-    $('#delivery-type').on('change', function () {
-        var value = $(this).find(':selected').data('option');
+    $('.options-deliver-method .btn-option').on('click',function() {
+        var value = $(this).data('option');
+        $('.options-deliver-method .btn-option').removeClass('btn-option--active');
+        $(this).addClass('btn-option--active');
         changeDeliveryMethod(value);
+        $("#delivery-type").val(value);
     });
     /**
      * Seleciona a forma de entrega na tela de meu cadastro
      */
-    $('input[type=radio][name="delivery-type"]').on('click', function () {
+    $('input[type=radio][name="delivery-type"]').on('click',function() {
         var value = $(this).data('option');
         changeDeliveryMethod(value);
     });
@@ -105,7 +120,7 @@ $(function () {
     /**
      *  Iniciar o processo de compra
      */
-    $('#start-checkout').on('click', function () {
+    $('#start-checkout').on('click',function() {
         $(this).addClass('hide');
         $('.checkout-info').removeClass('hide');
     });
@@ -113,12 +128,12 @@ $(function () {
     /**
      *  Alterar a forma de pagamento
      */
-    $('.payment-method input[type=radio]').on('click', function () {
+    $('.payment-method input[type=radio]').on('click',function() {
         var value = $(this).data('option');
-        if (value === 'CC') {
+        if(value === 'CC') {
             $('#payment-method-cc').removeClass('hide');
             $('#payment-method-boleto').addClass('hide');
-        } else if (value === 'Boleto') {
+        } else if(value === 'Boleto') {
             $('#payment-method-cc').addClass('hide');
             $('#payment-method-boleto').removeClass('hide');
         }
@@ -127,13 +142,13 @@ $(function () {
     /**
      *  Ativar radio buttons
      */
-    $('.form--radio .form--radio--option').on('click', function () {
+    $('.form--radio .form--radio--option').on('click',function() {
         var elm = $(this),
             parent = elm.closest('.form--radio');
-        parent.find('.form--radio--input.checked input').prop('checked', false);
+        parent.find('.form--radio--input.checked input').prop('checked',false);
         parent.find('.form--radio--input.checked').removeClass('checked');
         elm.find('.form--radio--input').addClass('checked');
-        elm.find('.form--radio--input.checked input').prop('checked', true);
+        elm.find('.form--radio--input.checked input').prop('checked',true);
     });
 
     /**
@@ -152,33 +167,49 @@ $(function () {
     /**
      * Ativar smooth scroll
      */
-    $('a[href*="#"]').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    $('a[href*="#"]').click(function() {
+        if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
             || location.hostname == this.hostname) {
-            if (this.hash.substr(1)) {
+            if(this.hash.substr(1)) {
                 var target = $('[name=' + this.hash.substr(1) + ']');
                 target = target.length ? target : $('[name=' + this.hash.substr(1) + ']');
-                if (target.length) {
+                if(target.length) {
                     $('html,body').animate({
                         scrollTop: target.offset().top - 120
-                    }, 1000);
+                    },1000);
                 }
             }
         }
     });
 });
-$(window).ready(function () {
+$(window).ready(function() {
     var url_page = window.location.href;
     var hashs = url_page.split('#');
-    if (typeof hashs != 'undefined' && hashs.length > 1) {
+    if(typeof hashs != 'undefined' && hashs.length > 1) {
         var rest = hashs[1];
-        if (rest) {
+        if(rest) {
             var target = $('[name=' + rest + ']');
-            if (target.length) {
+            if(target.length) {
                 $('html,body').animate({
                     scrollTop: target.offset().top - 40
-                }, 1000);
+                },1000);
             }
         }
     }
 });
+
+
+
+function showModal(modalSelector) {
+    $('body').css('overflow','hidden');
+    $(modalSelector).show(200);
+}
+
+function hideModal() {
+    $('body').css('overflow','auto');
+    if($(this).data('target')) {
+        $($(this).data('target')).hide(200);
+    } else {
+        $('.overlay').hide(200);
+    }    
+}
